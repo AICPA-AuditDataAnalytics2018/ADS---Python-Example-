0823db729ea10e67ec46c4a16f30d8204be6aa6b
mrcrnkovich <mrcrnkovich@gmail.com>
2020-01-19 22:21:00 -0500

    Refactoring Test Procedures and Adding Automated Tests for refactor

--- samples/Test_Procedures.py
+++ samples/Test_Procedures.py
@@ -1,6 +1,7 @@
 import csv
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
 # Decorator for printing function results. We return a results value to enable
 # automated testing of the methods upon refactoring.
@@ -55,6 +56,7 @@ class Test_1_Procedures:
 
 
     # 3.1.2 Compare listing of journal entry numbers from system to log file
+    
     @output_decorator()
     def comparison_of_entries_of_GL_and_log_file(
             GL_Detail, Log_File, Journal_ID_Column = 'Journal_ID',
@@ -78,6 +80,7 @@ class Test_1_Procedures:
                 "output": output_file})
 
     # 3.1.3 Test 1.3 Compare total debit amounts and credit amounts of journal entries to system control totals by entry type
+    
     @output_decorator()
     def comparison_of_amounts_of_GL_and_log_file(
             GL_Detail, Log_File, Journal_ID_Column = 'Journal_ID',
@@ -106,118 +109,157 @@ class Test_1_Procedures:
 class Test_2_Procedures:    
     # 3.2.1 - Examine population for missing or incomplete journal entries
     # Pivot by Journal_ID and make sure Net is 0 for each Journal ID, to check if debits and credits are equal for each entry
-    def check_for_incomplete_entries(GL_Detail_YYYYMMDD_YYYYMMDD,
-    output_file='', Journal_ID_Column = 'Journal_ID'):
+
+    @output_decorator()
+    def check_for_incomplete_entries(GL_Detail,
+                output_file='Output_Folder/Test_3_2_1_check_for_incomplete_entries.csv',
+                Journal_ID_Column = 'Journal_ID'):
         
-        GL_Pivot = GL_Detail_YYYYMMDD_YYYYMMDD.pivot_table(index=Journal_ID_Column, values='Net', aggfunc=sum)
+        GL_Pivot = GL_Detail.pivot_table(index=Journal_ID_Column, values='Net', aggfunc=sum)
         failed_test = GL_Pivot.loc[round(GL_Pivot['Net'], 2) != 0]
         failed_test = pd.DataFrame(failed_test.to_records())
         
         if output_file:
-            failed_test.to_csv('Output_Folder/Test_3_2_1_check_for_incomplete_entries.csv')
+            failed_test.to_csv(output_file)
         
         return ({"results": len(failed_test[Journal_ID_Column]), "output": output_file})
 
     # 3.2.2 - Examine possible duplicate account entries
     # Check for Journal Entries that have same account and amount in the same period
-    def check_for_duplicate_entries(GL_Detail_YYYYMMDD_YYYYMMDD):
-        print('Checking for Duplicate Entries is started')
-        import pandas as pd
-        import numpy as np
-        GL_Pivot = GL_Detail_YYYYMMDD_YYYYMMDD.pivot_table(index=['GL_Account_Number', 'Period', 'Net'], 
-                                                            values='Journal_ID', aggfunc= np.count_nonzero)
+
+    @output_decorator()
+    def check_for_duplicate_entries(GL_Detail, Journal_ID_Column = 'Journal_ID',
+                output_file='Output_Folder/Test_3_2_2_check_for_duplicate_entries.csv'):
+        
+        GL_Pivot = GL_Detail.pivot_table(index=['GL_Account_Number', 'Period', 'Net'], 
+                                                            values=Journal_ID_Column, aggfunc= np.count_nonzero)
         GL_Pivot.columns = ['Journal_Entry_Count']
         Duplicates = GL_Pivot.loc[GL_Pivot['Journal_Entry_Count'] != 1]
         Duplicates = pd.DataFrame(Duplicates.to_records())
-        GL_Copy = GL_Detail_YYYYMMDD_YYYYMMDD[['Journal_ID', 'GL_Account_Number', 'Period', 'Net']].copy()
+        GL_Copy = GL_Detail[['Journal_ID', 'GL_Account_Number', 'Period', 'Net']].copy()
         failed_test = GL_Copy.merge(Duplicates, on = ['GL_Account_Number', 'Period', 'Net'], how = 'right').fillna(0)
-        failed_test.to_csv('Output_Folder/Test_3_2_2_check_for_duplicate_entries.csv')
-        print('%d instances detected' %len(failed_test['Journal_ID']))
-        print('Results saved at Output_Folder/Test_3_2_2_check_for_duplicate_entries.csv')
+        
+        if output_file:
+            failed_test.to_csv(output_file)
+        
+        return ({'results':len(failed_test[Journal_ID_Column]),
+                    'output':output_file})
 
-
-    #3.2.3 - Examine round-dollar entries
+    # 3.2.3 - Examine round-dollar entries
     # Devide Amounts by 1000 and look for remainder of 0 to check for journal entries with exact amounts in '000s
-    def check_for_round_dollar_entries(GL_Detail_YYYYMMDD_YYYYMMDD):
-        print('Checking for Round Dollar Entries is started')
-        GL_Copy = GL_Detail_YYYYMMDD_YYYYMMDD[['Journal_ID', 'GL_Account_Number', 'Period', 'Net']].copy()
+
+    @output_decorator()
+    def check_for_round_dollar_entries(GL_Detail,
+                Journal_ID_Column='Journal_ID',
+                output_file='Output_Folder/Test_3_2_3_check_for_round_dollar_entries.csv'):
+        
+        GL_Copy = GL_Detail[[Journal_ID_Column, 'GL_Account_Number', 'Period', 'Net']].copy()
         GL_Copy['1000s Remainder'] = GL_Copy['Net'] % 1000
         failed_test = GL_Copy.loc[GL_Copy['1000s Remainder'] == 0] 
-        failed_test.to_csv('Output_Folder/Test_3_2_3_check_for_round_dollar_entries.csv')
-        print('%d instances detected' %len(failed_test['Journal_ID']))
-        print('Results saved at Output_Folder/Test_3_2_3_check_for_round_dollar_entries.csv')
+        
+        if output_file:
+            failed_test.to_csv(output_file)
 
+        return ({'results':len(failed_test[Journal_ID_Column]),
+                    'output':output_file})
 
-    #3.2.4 - Examine post-date entries:
-    #Check if Document Date was later than Entry Date
-    #Document_Date does not appear in Data Standards
-    #optimize&clarify
-    def check_for_post_date_entries(GL_Detail_YYYYMMDD_YYYYMMDD):
-        print('Checking for Post Date Entries is started')
-        GL_Copy = GL_Detail_YYYYMMDD_YYYYMMDD[['Journal_ID', 'Document_Date', 'Entered_Date', 'Period', 'Net']].copy()
+    # 3.2.4 - Examine post-date entries:
+    # Check if Document Date was later than Entry Date
+    # Document_Date does not appear in Data Standards
+    # optimize&clarify
+
+    @output_decorator()
+    def check_for_post_date_entries(GL_Detail,
+                Journal_ID_Column='Journal_ID',
+                output_file='Output_Folder/Test_3_2_4_check_for_post_date_entries.csv'):
+
+        GL_Copy = GL_Detail[['Journal_ID', 'Document_Date', 'Entered_Date', 'Period', 'Net']].copy()
         failed_test = GL_Copy.loc[GL_Copy['Document_Date'] > (GL_Copy['Entered_Date'] + 100)] #optimize&"accurify"
-        failed_test.to_csv('Output_Folder/Test_3_2_4_check_for_post_date_entries.csv')
-        print('%d instances detected' %len(failed_test['Journal_ID']))
-        print('Results saved at Output_Folder/Test_3_2_4_check_for_post_date_entries.csv')
+   
+        if output_file:
+            failed_test.to_csv(output_file)
+    
+        return ({'results':len(failed_test[Journal_ID_Column]),
+                    'output':output_file})
 
 
-    #3.2.5 - Examine entries posted on weekends/nights
-
+    # 3.2.5 - Examine entries posted on weekends/nights
     # Check if Entry Date falls on Saturday or Sunday
-    def check_for_weekend_entries(GL_Detail_YYYYMMDD_YYYYMMDD):
-        print('Checking for Weekend Entries is started')
-        from datetime import datetime
-        import pandas as pd
-        GL_Copy = GL_Detail_YYYYMMDD_YYYYMMDD[['Journal_ID', 'Entered_Date', 'Entered_Time']].copy()
+    
+    @output_decorator()
+    def check_for_weekend_entries(GL_Detail,
+                Journal_ID_Column='Journal_ID',
+                output_file='Output_Folder/Test_3_2_5.1_check_for_weekend_entries.csv'):
+        
+        GL_Copy = GL_Detail[[Journal_ID_Column, 'Entered_Date', 'Entered_Time']].copy()
         GL_Copy['Entry_Date_Time_Formatted'] = pd.to_datetime(GL_Copy['Entered_Date'].astype(str) + 
                                                               GL_Copy['Entered_Time'].astype(str), format='%Y%m%d%H%M%S')
         GL_Copy['WeekDayNo'] = GL_Copy['Entry_Date_Time_Formatted'].apply(lambda x: x.isoweekday())
         failed_test = GL_Copy.loc[GL_Copy['WeekDayNo'] >= 6]
-        failed_test.to_csv('Output_Folder/Test_3_2_5.1_check_for_weekend_entries.csv')
-        print('%d instances detected' %len(failed_test['Journal_ID']))
-        print('Results saved at Output_Folder/Test_3_2_5.1_check_for_weekend_entries.csv')
+        
+        if output_file:
+            failed_test.to_csv(output_file)
+
+        return ({'results':len(failed_test[Journal_ID_Column]),
+                    'output':output_file})
 
     # Check if Entry Time falls on between 8pm and 6am
-    def check_for_nights_entries(GL_Detail_YYYYMMDD_YYYYMMDD):
-        print('Checking for Night Entries is started')
-        from datetime import datetime
-        GL_Copy = GL_Detail_YYYYMMDD_YYYYMMDD[['Journal_ID', 'Entered_Date', 'Entered_Time']].copy()
+
+    @output_decorator()
+    def check_for_nights_entries(GL_Detail,
+                Journal_ID_Column='Journal_ID',
+                output_file='Output_Folder/Test_3_2_5.2_check_for_nights_entries.csv'):
+
+        GL_Copy = GL_Detail[['Journal_ID', 'Entered_Date', 'Entered_Time']].copy()
         GL_Copy['Entry_Date_Time_Formatted'] = pd.to_datetime(GL_Copy['Entered_Date'].astype(str) + 
                                                               GL_Copy['Entered_Time'].astype(str), format='%Y%m%d%H%M%S')
         GL_Copy['Hour'] = GL_Copy['Entry_Date_Time_Formatted'].dt.hour
         failed_test = GL_Copy.loc[(GL_Copy['Hour'] >= 20) | (GL_Copy['Hour'] <= 5)]
-        failed_test.to_csv('Output_Folder/Test_3_2_5.2_check_for_nights_entries.csv')
-        print('%d instances detected' %len(failed_test['Journal_ID']))
-        print('Results saved at Output_Folder/Test_3_2_5.2_check_for_nights_entries.csv')
+        
+        if output_file:
+            failed_test.to_csv(output_file)
 
+        return ({'results':len(failed_test[Journal_ID_Column]),
+                    'output':output_file})
 
-    #3.2.6 - Summarize by person, type and period in order to identify individuals who normally do not post entries, 
-    #and to identify accounts that are normally not used.
-
-    #Check for individuals who posted 10 or fewer entries and identify entries made by these individuals
-    def check_for_rare_users(GL_Detail_YYYYMMDD_YYYYMMDD):
-
-        print('Checking for Rare Users is started')
-        GL_Pivot = GL_Detail_YYYYMMDD_YYYYMMDD.pivot_table(index=['Entered_By'], values='Journal_ID', 
+    # 3.2.6 - Summarize by person, type and period in order to identify individuals who normally do not post entries, 
+    # and to identify accounts that are normally not used.
+    # Check for individuals who posted 10 or fewer entries and identify entries made by these individuals
+    
+    @output_decorator()
+    def check_for_rare_users(GL_Detail,
+                Journal_ID_Column='Journal_ID',
+                output_file='Output_Folder/Test_3_2_6.1_check_for_rare_users.csv'):
+    
+        GL_Pivot = GL_Detail.pivot_table(index=['Entered_By'], values='Journal_ID', 
                                                            aggfunc=np.count_nonzero).fillna(0)
         Rare_Users = GL_Pivot.loc[GL_Pivot['Journal_ID'] <= 10]
         Rare_Users = pd.DataFrame(Rare_Users.to_records())
-        GL_Copy = GL_Detail_YYYYMMDD_YYYYMMDD[['Journal_ID', 'GL_Account_Number', 'Entered_By']].copy()
+        GL_Copy = GL_Detail[['Journal_ID', 'GL_Account_Number', 'Entered_By']].copy()
         failed_test = GL_Copy.merge(Rare_Users, on = ['Entered_By'], how = 'right').fillna(0)
-        failed_test.to_csv('Output_Folder/Test_3_2_6.1_check_for_rare_users.csv')
-        print('%d instances detected' %len(failed_test['Entered_By']))
-        print('Results saved at Output_Folder/Test_3_2_6.1_check_for_rare_users.csv')
+        
+        if output_file:
+            failed_test.to_csv(output_file)
+
+        return ({'results':len(failed_test['Entered_By']),
+                    'output':output_file})
 
     # Check for accounts that were used 3 or fewer times and identify entries made to these accounts
-    def check_for_rare_accounts(GL_Detail_YYYYMMDD_YYYYMMDD):
+    
+    @output_decorator()
+    def check_for_rare_accounts(GL_Detail,
+                Journal_ID_Column='Journal_ID',
+                output_file='Output_Folder/Test_3_2_6.2_check_for_rare_accounts.csv'):
 
-        print('Checking for Rare Accounts is started')
-        GL_Pivot = GL_Detail_YYYYMMDD_YYYYMMDD.pivot_table(index=['GL_Account_Number'], values='Journal_ID', 
+        GL_Pivot = GL_Detail.pivot_table(index=['GL_Account_Number'], values='Journal_ID', 
                                                             aggfunc=np.count_nonzero).fillna(0)
         Rare_Accounts = GL_Pivot.loc[GL_Pivot['Journal_ID'] <= 3]
         Rare_Accounts = pd.DataFrame(Rare_Accounts.to_records())
-        GL_Copy = GL_Detail_YYYYMMDD_YYYYMMDD[['Journal_ID', 'GL_Account_Number', 'Entered_By']].copy()
+        GL_Copy = GL_Detail[['Journal_ID', 'GL_Account_Number', 'Entered_By']].copy()
         failed_test = GL_Copy.merge(Rare_Accounts, on = ['GL_Account_Number'], how = 'right').fillna(0)
-        failed_test.to_csv('Output_Folder/Test_3_2_6.2_check_for_rare_accounts.csv')
-        print('%d instances detected' %len(failed_test['GL_Account_Number']))
-        print('Results saved at Output_Folder/Test_3_2_6.2_check_for_rare_accounts.csv')
+        
+        if output_file:
+            failed_test.to_csv(output_file)
+        
+        return ({'results':len(failed_test['GL_Account_Number']),
+                    'output':output_file})
--- tests/test_JE_procedures.py
+++ tests/test_JE_procedures.py
@@ -1,39 +1,71 @@
 import pandas as pd
 import pytest
-from samples.Test_Procedures import Test_1_Procedures
+from samples.Test_Procedures import Test_1_Procedures, Test_2_Procedures
 
+
+df_log_sample = pd.read_csv("../samples/data/log_file.csv")
+df_gl_sample = pd.read_csv("../samples/data/"+
+                                "GL_Detail_YYYYMMDD_YYYYMMDD.csv")
+
+df_gl_sample['Net'] = df_gl_sample.apply(lambda x: x['Amount'] * -1 if
+        x['Amount_Credit_Debit_Indicator']=='H' else x['Amount'], axis=1)
 
 class Test_Procedure_1:
-    def test_check_gl_entries(self):
-        df_gl_sample = pd.read_csv("../samples/data/"+
-                                "GL_Detail_YYYYMMDD_YYYYMMDD.csv")
 
-        result = Test_1_Procedures.check_for_gaps_in_JE_ID(df_gl_sample,
+    def test_check_gl_entries(self):
+
+        assert 12 == Test_1_Procedures.check_for_gaps_in_JE_ID(df_gl_sample,
                         Journal_ID_Column = "Journal_ID", output_file = None)
 
-        assert result == 12
-
     def test_comparison_of_entries_of_GL_and_log_files(self):
-        df_log_sample = pd.read_csv("../samples/data/log_file.csv")
-        df_gl_sample = pd.read_csv("../samples/data/"+
-                                "GL_Detail_YYYYMMDD_YYYYMMDD.csv")
 
-        result = Test_1_Procedures.comparison_of_entries_of_GL_and_log_file(
-                   GL_Detail = df_gl_sample, Log_File = df_log_sample, 
-                    output_file = None)
+        assert 0 == Test_1_Procedures.comparison_of_entries_of_GL_and_log_file(
+                        GL_Detail = df_gl_sample, Log_File = df_log_sample, 
+                        output_file = None)
 
-        assert result == 0
-    
     def test_comparison_of_amounts_of_GL_and_log_files(self):
-        df_log_sample = pd.read_csv("../samples/data/log_file.csv")
-        df_gl_sample = pd.read_csv("../samples/data/"+
-                                "GL_Detail_YYYYMMDD_YYYYMMDD.csv")
 
-        df_gl_sample['Net'] = df_gl_sample.apply(lambda x: x['Amount'] * -1 if
-                x['Amount_Credit_Debit_Indicator']=='H' else x['Amount'], axis=1)
-
-        result = Test_1_Procedures.comparison_of_amounts_of_GL_and_log_file(
+        assert 2 == Test_1_Procedures.comparison_of_amounts_of_GL_and_log_file(
                     GL_Detail=df_gl_sample, Log_File=df_log_sample, 
                     output_file = None)
 
-        assert result == 2
+    def test_check_for_incomplete_entries(self):
+
+        assert 4 == Test_2_Procedures.check_for_incomplete_entries(
+                        GL_Detail = df_gl_sample, output_file = None)
+
+    def test_check_for_duplicate_entries(self):
+
+        assert 6919 == Test_2_Procedures.check_for_duplicate_entries(
+                            GL_Detail = df_gl_sample, output_file = None)
+
+    def test_check_for_round_dollar_entries(self):
+
+        assert 226 == Test_2_Procedures.check_for_round_dollar_entries(
+                            GL_Detail = df_gl_sample, output_file = None)
+
+    def test_check_for_post_date_entries(self):
+
+        assert 149 == Test_2_Procedures.check_for_post_date_entries(
+                            GL_Detail = df_gl_sample, output_file = None)
+
+    def test_check_for_weekend_entries(self):
+
+        assert 0 == Test_2_Procedures.check_for_weekend_entries(
+                            GL_Detail = df_gl_sample, output_file = None)
+
+    def test_check_for_nights_entries(self):
+
+        assert 190 == Test_2_Procedures.check_for_nights_entries(
+                            GL_Detail = df_gl_sample, output_file = None)
+
+    def test_check_for_rare_users(self):
+
+        assert 52 == Test_2_Procedures.check_for_rare_users(
+                            GL_Detail = df_gl_sample, output_file = None)
+
+    def test_check_for_rare_accounts(self):
+
+        assert 32 == Test_2_Procedures.check_for_rare_accounts(
+                            GL_Detail = df_gl_sample, output_file = None)
+
